ee6910f6bc1d3417d261a68d63cfc66f0082b6a0
sayyidinside <70059117+sayyidinside@users.noreply.github.com>
2024-09-12 13:46:49 +0700

    ➕ Create Routes Setup, Versioning, And User Management

--- interfaces/http/routes/router.go
+++ interfaces/http/routes/router.go
@@ -5,9 +5,14 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/sayyidinside/gofiber-clean-fresh/infrastructure/config"
+	"github.com/sayyidinside/gofiber-clean-fresh/interfaces/http/handlers"
+	v1 "github.com/sayyidinside/gofiber-clean-fresh/interfaces/http/routes/v1"
 )
 
-func Setup(app *fiber.App) {
+func Setup(app *fiber.App, handler *handlers.Handlers) {
+	api := app.Group("/api")
+	v1.RegisterRoutes(api, handler)
+
 	cfg := config.AppConfig
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString(fmt.Sprintf("App is running, with name %s", cfg.AppName))
