ab1cacb51aed061f6da73fb188d1d59934b23feb
sayyidinside <70059117+sayyidinside@users.noreply.github.com>
2024-10-03 15:14:47 +0700

    ➕ Create Logout, Refresh, And Verify

--- interfaces/http/handler/auth_handler.go
+++ interfaces/http/handler/auth_handler.go
@@ -23,8 +23,39 @@ func NewAuthHandler(service service.AuthService) AuthHandler {
 }
 
 func (h *authHandler) Login(c *fiber.Ctx) error {
-	logData := helpers.CreateLog(c)
+	logDataData := helpers.CreateLog(c)
 	var input model.LoginInput
+
+	if err := c.BodyParser(&input); err != nil {
+		return helpers.ResponseFormatter(c, helpers.BaseResponse{
+			Status:  fiber.StatusBadRequest,
+			Success: false,
+			Message: "Invalid or malformed request body",
+			Log:     &logDataData,
+		})
+	}
+
+	if err := helpers.ValidateInput(input); err != nil {
+		return helpers.ResponseFormatter(c, helpers.BaseResponse{
+			Status:  fiber.StatusBadRequest,
+			Success: false,
+			Message: "Invalid or malformed request body",
+			Errors:  err,
+			Log:     &logDataData,
+		})
+	}
+
+	model.SanitizeLoginInput(&input)
+
+	response := h.service.Login(c.Context(), &input)
+	response.Log = &logDataData
+
+	return helpers.ResponseFormatter(c, response)
+}
+
+func (h *authHandler) Logout(c *fiber.Ctx) error {
+	logData := helpers.CreateLog(c)
+	var input model.TokenInput
 
 	if err := c.BodyParser(&input); err != nil {
 		return helpers.ResponseFormatter(c, helpers.BaseResponse{
@@ -34,6 +65,8 @@ func (h *authHandler) Login(c *fiber.Ctx) error {
 			Log:     &logData,
 		})
 	}
+
+	model.SanitizeTokenInput(&input)
 
 	if err := helpers.ValidateInput(input); err != nil {
 		return helpers.ResponseFormatter(c, helpers.BaseResponse{
@@ -45,25 +78,73 @@ func (h *authHandler) Login(c *fiber.Ctx) error {
 		})
 	}
 
-	model.SanitizeLoginInput(&input)
-
-	response := h.service.Login(c.Context(), &input)
+	response := h.service.Logout(c.Context(), input.Token)
 	response.Log = &logData
 
 	return helpers.ResponseFormatter(c, response)
 }
 
-func (h *authHandler) Logout(c *fiber.Ctx) error {
-	// log := helpers.CreateLog(c)
-	return nil
-}
-
 func (h *authHandler) Refresh(c *fiber.Ctx) error {
-	// log := helpers.CreateLog(c)
-	return nil
+	logData := helpers.CreateLog(c)
+	var input model.TokenInput
+
+	if err := c.BodyParser(&input); err != nil {
+		return helpers.ResponseFormatter(c, helpers.BaseResponse{
+			Status:  fiber.StatusBadRequest,
+			Success: false,
+			Message: "Invalid or malformed request body",
+			Log:     &logData,
+		})
+	}
+
+	model.SanitizeTokenInput(&input)
+
+	if err := helpers.ValidateInput(input); err != nil {
+		return helpers.ResponseFormatter(c, helpers.BaseResponse{
+			Status:  fiber.StatusBadRequest,
+			Success: false,
+			Message: "Invalid or malformed request body",
+			Errors:  err,
+			Log:     &logData,
+		})
+	}
+
+	response := h.service.Refresh(c.Context(), input.Token)
+	response.Log = &logData
+
+	return helpers.ResponseFormatter(c, response)
 }
 
 func (h *authHandler) Verify(c *fiber.Ctx) error {
-	// log := helpers.CreateLog(c)
-	return nil
+	logData := helpers.CreateLog(c)
+	var input model.TokenInput
+
+	if err := c.BodyParser(&input); err != nil {
+		return helpers.ResponseFormatter(c, helpers.BaseResponse{
+			Status:  fiber.StatusBadRequest,
+			Success: false,
+			Message: "Invalid or malformed request body",
+			Log:     &logData,
+		})
+	}
+
+	model.SanitizeTokenInput(&input)
+
+	if err := helpers.ValidateInput(input); err != nil {
+		return helpers.ResponseFormatter(c, helpers.BaseResponse{
+			Status:  fiber.StatusBadRequest,
+			Success: false,
+			Message: "Invalid or malformed request body",
+			Errors:  err,
+			Log:     &logData,
+		})
+	}
+
+	response := h.service.VerifyAccessToken(c.Context(), input.Token)
+	if !response.Success {
+		response = h.service.VerifyRefreshToken(c.Context(), input.Token)
+	}
+	response.Log = &logData
+
+	return helpers.ResponseFormatter(c, response)
 }
--- interfaces/model/auth_model.go
+++ interfaces/model/auth_model.go
@@ -7,6 +7,10 @@ type (
 		UsernameOrEmail string `json:"username_or_email" form:"username_or_email" xml:"username_or_email" validate:"required"`
 		Password        string `json:"password" form:"password" xml:"password" validate:"required"`
 	}
+
+	TokenInput struct {
+		Token string `json:"token" form:"token" xml:"token" validate:"required"`
+	}
 )
 
 func SanitizeLoginInput(input *LoginInput) {
@@ -14,4 +18,10 @@ func SanitizeLoginInput(input *LoginInput) {
 
 	input.UsernameOrEmail = sanitizer.Sanitize(input.UsernameOrEmail)
 	input.Password = sanitizer.Sanitize(input.Password)
+}
+
+func SanitizeTokenInput(input *TokenInput) {
+	sanitizer := bluemonday.StrictPolicy()
+
+	input.Token = sanitizer.Sanitize(input.Token)
 }
